a915c6c4f122623295a87b43790d279fcd415849
Nicolas Aversa <nicolasaversadm@gmail.com>
2020-07-04 12:58:35 -0300

    Basic events

--- react-tutos/src/components/Person/Person.jsx
+++ react-tutos/src/components/Person/Person.jsx
@@ -6,11 +6,12 @@ const Person = (props) => {
   return (
     <Card className="mb-3">
       <Card.Body>
-        <Card.Title>{props.name}</Card.Title>
+        <Card.Title onClick={props.click}>{props.name}</Card.Title>
         <Card.Subtitle className="mb-2 text-muted">
           Edad: {props.age}
         </Card.Subtitle>
         <Card.Text>{props.children} </Card.Text>
+        <input type="text" onChange={props.change} defaultValue={props.name} />
       </Card.Body>
     </Card>
   );
--- react-tutos/src/containers/Home/Home.jsx
+++ react-tutos/src/containers/Home/Home.jsx
@@ -10,38 +10,56 @@ const Home = () => {
     { name: "Juan Manuel", age: 26 },
     { name: "Florencia", age: 28 },
   ]);
+  const [showPersons, setShowPersons] = useState(false);
 
-  const switchNameHandler = () => {
+  const switchNameHandler = (newName) => {
     let newPersons = [...persons];
-    newPersons[1].name = "German";
+    newPersons[1].name = newName;
     setPersons(newPersons);
+  };
+
+  const nameChangedHandler = (event) => {
+    let newPersons = [...persons];
+    newPersons[1].name = event.target.value;
+    setPersons(newPersons);
+  };
+
+  const togglePersonsHandler = () => {
+    setShowPersons(!showPersons);
   };
 
   return (
     <>
       <Col xs={12} className="mb-3">
         <h1>This is a React app, Hope you like it!</h1>
-        <h4>Let's start the adventure!</h4>
-      </Col>
-      <Col xs={4}>
-        <Person name={persons[0].name} age={persons[0].age} />
-      </Col>
-      <Col xs={4}>
-        <Person name={persons[1].name} age={persons[1].age} />
-      </Col>
-      <Col xs={4}>
-        <Person name={persons[2].name} age={persons[2].age} />
-      </Col>
-      <Col xs={4}>
-        <Person name="Leonel" age="25">
-          Me gusta la papa!
-        </Person>
-      </Col>
-      <Col xs={12}>
-        <Button onClick={switchNameHandler} variant="primary">
-          Hacer algo
+        <h4 className="mb-3">Let's start the adventure!</h4>
+        <Button onClick={togglePersonsHandler} variant="primary">
+          Alternar visibilidad
         </Button>
       </Col>
+      {showPersons ? (
+        <>
+          <Col xs={4}>
+            <Person name={persons[0].name} age={persons[0].age} />
+          </Col>
+          <Col xs={4}>
+            <Person
+              name={persons[1].name}
+              age={persons[1].age}
+              click={() => switchNameHandler("Pepito")}
+              change={nameChangedHandler}
+            />
+          </Col>
+          <Col xs={4}>
+            <Person name={persons[2].name} age={persons[2].age} />
+          </Col>
+          <Col xs={4}>
+            <Person name="Leonel" age="25">
+              Me gusta la papa!
+            </Person>
+          </Col>
+        </>
+      ) : null}
     </>
   );
 };
