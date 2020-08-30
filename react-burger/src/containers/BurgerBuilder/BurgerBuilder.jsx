16997de721b28eb8ee7d39a7544ba40a715fb10f
Nicolas Aversa <nicolasaversamm2@gmail.com>
2020-08-29 21:52:27 -0300

    BurgerIngredients added

---
+++ react-burger/src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Burger from '../../components/Burger/Burger';
+
+function BurgerBuilder() {
+  return (
+    <>
+      <div>Burger</div>
+      <Burger />
+    </>
+  );
+}
+
+export default BurgerBuilder;
