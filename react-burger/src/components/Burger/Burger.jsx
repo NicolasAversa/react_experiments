0987f1d9324e131975da3ee853d3973589f5a167
Nicolas Aversa <nicolasaversamm2@gmail.com>
2020-09-04 18:36:27 -0300

    Ingredients list added

--- react-burger/src/components/Burger/Burger.jsx
+++ react-burger/src/components/Burger/Burger.jsx
@@ -1,16 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './Burger.module.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
-function Burger() {
+const propTypes = {
+  ingredients: PropTypes.shape({
+    salad: PropTypes.number,
+    bacon: PropTypes.number,
+    cheese: PropTypes.number,
+    meat: PropTypes.number,
+  }).isRequired,
+};
+
+function Burger(props) {
+  const { ingredients } = props;
+  const transformedIngredients = Object.keys(ingredients).map((key) =>
+    [...Array(ingredients[key])].map((_, i) => <BurgerIngredient key={key + i} type={key} />),
+  );
+
   return (
     <div className={styles.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="cheese" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
 }
+
+Burger.propTypes = propTypes;
 
 export default Burger;
--- react-burger/src/containers/BurgerBuilder/BurgerBuilder.jsx
+++ react-burger/src/containers/BurgerBuilder/BurgerBuilder.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Burger from '../../components/Burger/Burger';
 
 function BurgerBuilder() {
+  const [ingredients, setIngredients] = useState({
+    salad: 1,
+    bacon: 1,
+    cheese: 2,
+    meat: 2,
+  });
+
   return (
     <>
       <div>Burger</div>
-      <Burger />
+      <Burger ingredients={ingredients} />
     </>
   );
 }
